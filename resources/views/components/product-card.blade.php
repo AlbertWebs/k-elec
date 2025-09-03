eaa3bf951f86166d47e5cacfd86649181f9a6ed4
AlbertWebs <albertmuhatia@gmail.com>
2025-09-03 19:05:50 +0300

    update changes in product card overlaping

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -73,7 +73,7 @@
             <meta itemprop="worstRating" content="1">
         </div> --}}
 
-         <a href="{{ route('products.show', $product->slug) }}" class="inline-blocks border-2 border-gray-900 text-gray-900 px-1 py-1 rounded-lg font-semibold hover:bg-gray-900 hover:text-white transition-colors text-center mobile-btn" >
+         <a href="{{ route('products.show', $product->slug) }}" class="inline-block border-2 border-gray-900 text-gray-900 px-1 py-1 rounded-lg font-semibold hover:bg-gray-900 hover:text-white transition-colors text-center mb-2 mobile-btn" >
             View Product →
         </a>
 

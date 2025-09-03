233a0da37392d54e9877dccb9988bccfc604ab70
AlbertWebs <albertmuhatia@gmail.com>
2025-09-03 19:11:32 +0300

    update min heaight

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -54,7 +54,7 @@
     <div class="p-3 sm:p-4 flex flex-col">
         <!-- Product Name with Link -->
         <a href="{{ route('products.show', $product->slug) }}" class="">
-            <h3 class="text-xs sm:text-sm font-medium text-gray-900 hover:text-blue-600 transition-colors line-clamp-2 text-center mb-2" style="height:40px;" itemprop="name">{{ $product->name }}</h3>
+            <h3 class="text-xs sm:text-sm font-medium text-gray-900 hover:text-blue-600 transition-colors line-clamp-2 text-center mb-2" style="min-height:40px;" itemprop="name">{{ $product->name }}</h3>
         </a>
         
         <!-- Rating -->

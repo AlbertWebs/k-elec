42df7510eb558f22a45d4c7c0ee0e717b974083d
AlbertWebs <albertmuhatia@gmail.com>
2025-09-04 09:54:32 +0300

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -84,7 +84,7 @@ use App\Helpers\SocialMediaHelper;
                     <li><a href="{{ route('home') }}" class="text-gray-400 hover:text-white">Home </a></li>
                     <li><a href="{{ route('pages.about') }}" class="text-gray-400 hover:text-white">About Us</a></li>
                     <li><a href="{{ route('pages.contact') }}" class="text-gray-400 hover:text-white">Contact Us</a></li>
-                    <li><a href="{{ route('showrooms.index') }}" class="text-gray-400 hover:text-white">Our Showrooms</a></li>
+                    {{-- <li><a href="{{ route('showrooms.index') }}" class="text-gray-400 hover:text-white">Our Showrooms</a></li> --}}
                     <li><a href="{{ route('products.index') }}" class="text-gray-400 hover:text-white">Our Products</a></li>
                     {{-- <li><a href="{{ route('pages.technical-support') }}" class="text-gray-400 hover:text-white">Technical Support</a></li>
                     <li><a href="{{ route('pages.shipping-returns') }}" class="text-gray-400 hover:text-white">Shipping & Returns</a></li>

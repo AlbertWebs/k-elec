2547a35599399a0cc2c9cf0e1ec175169c938574
AlbertWebs <albertmuhatia@gmail.com>
2025-09-05 14:35:42 +0300

    update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -25,7 +25,7 @@
               @foreach ($carouselSlides as $index => $slide)
                 <div class="swiper-slide bg-gradient-to-r {{ $slide->background_classes }} 
                             p-4 lg:p-8 bg-cover bg-center bg-no-repeat flex items-center justify-center h-full"
-                  style="background-image: url('{{url('/')}}/storage/{{$slide->image}}'); ">
+                  style="background-image: url('{{url('/')}}/storage/{{$slide->image}}'); background-size:  cover; background-position: center;">
                   
                   <div class="text-center max-w-2xl">
                     <img src="{{ asset('images/logo.png') }}" alt="K-ELEC" 

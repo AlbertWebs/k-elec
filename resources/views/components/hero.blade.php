20523a16c1667f757843c12e191d2b3363964c9b
AlbertWebs <albertmuhatia@gmail.com>
2025-09-11 10:33:18 +0300

    update

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -25,7 +25,7 @@
             
             <div class="swiper-wrapper">
               @foreach ($carouselSlides as $index => $slide)
-                <a href="{{$slide->button_link}}">
+                
                   <div class="swiper-slide bg-gradient-to-r {{ $slide->background_classes }} 
                               p-4 lg:p-8 bg-cover bg-center bg-no-repeat flex items-center justify-center h-full"
                     style="background-image: url('{{url('/')}}/storage/{{$slide->image}}'); background-size:  cover; background-position: center;">
@@ -43,7 +43,7 @@
                       </p>
                     </div>
                   </div>
-                </a>
+              
               @endforeach
             </div>
 
